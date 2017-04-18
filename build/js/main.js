62a78ca470f1c7f863099c44f577b53e21e5db7b
RobLowe1992 <RobLowe1992@yahoo.com>
2017-04-18 17:53:20 -0400

    created game logic

---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+"use strict";console.log("Up and running!");var cardOne="Queen",cardTwo="Queen",cardThree="King",cardFour="King";console.log("User flipped "+cardOne),console.log("User flipped "+cardThree);
---
+++ js/main.js
@@ -0,0 +1,12 @@
+// @codekit-prepend 'js/main.js'
+
+console.log("Up and running!");
+
+var cardOne = "Queen",
+    cardTwo = "Queen",
+    cardThree = "King",
+    cardFour = "King";
+
+console.log(`User flipped ${cardOne}`);
+console.log(`User flipped ${cardThree}`);
+
